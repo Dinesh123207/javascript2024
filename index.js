72d713f9c86a226579ad811e16aa1e7b07c218be
Dinesh Kumar <66234432+Dinesh123207@users.noreply.github.com>
2024-02-17 19:41:30 +0530

    2.1

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-const fs = require("fs");
-function callbackFn(err,data) {
-    console.log(data);
+// const fs = require("fs");
+// function callbackFn(err,data) {
+//     console.log(data);
+
+// }
+// fs.readFile("a.txt","utf-8", callbackFn);
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+function handleFirstRequest(req, res){
+    res.send('hello world');
+
 
 }
-fs.readFile("a.txt","utf-8", callbackFn);
+app.get('/', handleFirstRequest)
 
-// function calculatesum(counter){
-//     var sum =0;
-//     for(let i=0;i<counter;i++){
-//         sum = sum + i;
-//     }
-//     return sum;
-// }
-// var calculatedsum = calculatesum(100);
-// console.log(calculatedsum);
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+function calculatesum(counter){
+    var sum =0;
+    for(let i=0;i<counter;i++){
+        sum = sum + i;
+    }
+    return sum;
+}
+var calculatedsum = calculatesum(100);
+console.log(calculatedsum);
