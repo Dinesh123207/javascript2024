0bb4962e211b7ba5e3fbd7ada88b85837b028966
Dinesh Kumar <66234432+Dinesh123207@users.noreply.github.com>
2024-02-17 18:02:22 +0530

    week 2.1(a)

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-function calculatesum(counter){
-    var sum =0;
-    for(let i=0;i<counter;i++){
-        sum = sum + i;
-    }
-    return sum;
+const fs = require("fs");
+function callbackFn(err,data) {
+    console.log(data);
+
 }
-var calculatedsum = calculatesum(100);
-console.log(calculatedsum);
+fs.readFile("a.txt","utf-8", callbackFn);
+
+// function calculatesum(counter){
+//     var sum =0;
+//     for(let i=0;i<counter;i++){
+//         sum = sum + i;
+//     }
+//     return sum;
+// }
+// var calculatedsum = calculatesum(100);
+// console.log(calculatedsum);
